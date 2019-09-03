53fa1ce52e20d31e8c33a75a8e2bd8fdc7eb5643
slisovski <simeon.lisovski@vogelwarte.ch>
2019-09-03 22:09:14 +0200

    NightSession

--- app.R
+++ app.R
@@ -6,14 +6,12 @@ library(SGAT)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  navbarPage("VWSGapp",
+  navbarPage("VWSGapp", id = "navbar",
              
              tabPanel("Light data",
-                      
 
-                      titlePanel("Select your data"),
+                      titlePanel("Select raw light recordings"),
                       
-
                       sidebarLayout(
                         sidebarPanel(
                           radioButtons("filetype", 
@@ -28,6 +26,10 @@ ui <- fluidPage(
                                                ".lig")
                           ),
                           
+                          numericInput("offset", 
+                                       label = "Offset", 
+                                       value = 1,  #default value
+                                       step  = 1), #"steps" with arrow buttons.
                           br(),
                           
                           numericInput("lon.calib", 
@@ -38,35 +40,67 @@ ui <- fluidPage(
                                        label = "Deployment latitude",  
                                        value = NULL,
                                        step = 0.00001),
+                            
                           conditionalPanel(
                             condition = "output.dateSlider == true",
                             sliderInput("Dates",
                                       "Dates:",
-                                      min   = as.Date("2016-01-01","%Y-%m-%d"),
-                                      max   = as.Date("2016-12-01","%Y-%m-%d"),
-                                      value = c(as.Date("2016-02-01"), as.Date("2016-11-01")),
+                                      min   = as.Date("1970-01-01", "%Y-%m-%d"),
+                                      max   = as.Date("1970-12-30", "%Y-%m-%d"),
+                                      value = as.Date(c("1970-01-01", "1970-12-30"), "%Y-%m-%d"),
                                       timeFormat="%Y-%m-%d")
-                          )
+                            
+                        )
+                          
                         ),
                         
                         mainPanel(
                           plotOutput("lightImage0"),
                           plotOutput("lightSeries")
                         )
-                        
-                        
                 
-                
-                  ) ## end Sidebar Panel 
-             ) ## tabPanel
-             
-  )
+                  ) ## end Sidebar Panel
+                  
+             ), ## tabPanel (Data)
+             tabPanel("Twilight",
+                      
+                      titlePanel("Twilight Annotation"),
+                      
+                      sidebarLayout(
+                        sidebarPanel(
+                          numericInput("threshold", 
+                                       label = h4("Threshold"), 
+                                       value = 1,  #default value
+                                       step  = 0.25), #"steps" with arrow buttons.
+                          br(),
+                          
+                          conditionalPanel(
+                            condition = "output.dateSlider == true",
+                            h4("1. Step: Select range"),
+                            actionButton("Step1.1", "accept"),
+                            actionButton("Step1.2", "reset")
+                          )
+                        ),
+                        mainPanel(
+                          conditionalPanel(
+                            condition = "output.dateSlider == true",
+                                h4("Draw rectangle to select range"),
+                                uiOutput("selectRangePlot")
+                          )
+                          )
+                      )
+            ), ## tabPanel (Twilight)
+            tabPanel("Calibration"),
+            tabPanel("Breeding site"),
+            tabPanel("Breeding performance"),
+            tabPanel("Location estimates")
+            
+      ) ## navbarPage
+) ## ui
+
+
+server <- function(input, output, session) {
   
-)
-
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
   
   #########################
   #Read in a dataset from a file.
@@ -80,7 +114,9 @@ server <- function(input, output) {
     
     #Nesting ifelse shows what to do if inFile is null (no entry)
     #and what to do for each input radio button type.
-    if (is.null(inFile)) {return(NULL)} else
+    if (is.null(inFile)) {
+      return(NULL)
+      } else
     {
       if (input$filetype == ".lig") {
         return(NULL)
@@ -88,34 +124,72 @@ server <- function(input, output) {
       {
         if (input$filetype == ".lux") {
           
-          raw <- readMTlux(inFile$datapath) #read the data into a dataframe called d.lig
-          raw$Light<-log(raw$Light)
+          tab <- readMTlux(inFile$datapath) #read the data into a dataframe called d.lig
+          tab$Light<-log(tab$Light)
           
           output$dateSlider <- reactive(ncol(raw())>0)
-          outputOptions(output, c("dateSlider", "min"), suspendWhenHidden = FALSE)
+          outputOptions(output, c("dateSlider"), suspendWhenHidden = FALSE)
           
-          return(raw)
+          return(tab)
           
         } 
       }
     }
    })
   
+  observe({
+    if(!is.null(input$filename)) {
+      min <- format(raw()$Date[1], "%Y-%m-%d")
+      max <- format(raw()$Date[nrow(raw())], "%Y-%m-%d")
+    } else {
+      min <- "2000-01-01"
+      max <- "2025-12-31"
+    }
+    # Control the value, min, max, and step.
+    # Step size is 2 when input value is even; 1 when value is odd.
+    updateSliderInput(session, "Dates",
+                      min   = as.Date(min, "%Y-%m-%d"),
+                      max   = as.Date(max, "%Y-%m-%d"),
+                      value = as.Date(c(min, max), "%Y-%m-%d"))
+    updateSliderInput(session, "Range",
+                      min   = as.Date(min, "%Y-%m-%d"),
+                      max   = as.Date(max, "%Y-%m-%d"),
+                      value = as.Date(c(min, max), "%Y-%m-%d"))
+  })
   
   output$lightImage0 <- renderPlot({
     
     if(is.null(input$filename)) {
       plot(1,1, type = "n", xaxt = "n", yaxt = "n", bty = "n", xlab = "", ylab = "")
     } else {
-      offset <- 1
-      lightImage(raw(), offset = offset, zlim = c(0, 10), dt = 120)
+      lightImage(raw(), offset = input$offset, zlim = c(0, 10), dt = 120)
       
       if(all(!is.na(c(input$lon.calib, input$lat.calib)))) {
-        tsimageDeploymentLines(raw()$Date, input$lon.calib, input$lat.calib, offset = offset,
+        tsimageDeploymentLines(raw()$Date, input$lon.calib, input$lat.calib, offset = input$offset,
                                lwd = 4, col = adjustcolor("orange", alpha.f = 0.6))
       }
+      
+      abline(v = as.POSIXct(input$Dates, format = "%Y-%m-%d"), lty = 2, lwd = 2, col = adjustcolor("orange", alpha.f = 0.7))
     }
     
+  })
+  
+  
+  output$lightImage1 <- renderPlot({
+    
+    if(is.null(input$filename)) {
+      plot(1,1, type = "n", xaxt = "n", yaxt = "n", bty = "n", xlab = "", ylab = "")
+    } else {
+      lightImage(raw(), offset = input$offset, zlim = c(0, 10), dt = 120)
+    }
+    
+  })
+  
+  
+  output$selectRangePlot <- renderUI({
+    plotOutput("lightImage1", height=500,
+               brush = brushOpts(id = "select_range_brush")
+    )
   })
   
   
@@ -124,18 +198,16 @@ server <- function(input, output) {
     if(is.null(input$filename)) {
       plot(1,1, type = "n", xaxt = "n", yaxt = "n", bty = "n", xlab = "", ylab = "")
     } else {
-      plot(raw()$Date, raw()$Light, type = "o", pch = 16, cex = 0.25, xlab = "", ylab = "log light", las = 1)
+        
+      range <- as.POSIXct(raw()$Date) > as.POSIXct(input$Dates[1], format = "%Y-%m-%d") &
+               as.POSIXct(raw()$Date) < as.POSIXct(input$Dates[2], format = "%Y-%m-%d")
+      
+      with(raw()[range,], plot(Date, Light, type = "o", pch = 16, cex = 0.25, xlab = "", ylab = "log light", las = 1,
+                                 ylim = range(Light, na.rm = T)))
     }
     
   })
- 
-  
-   
 }
-
-
-
-
 
 
 
